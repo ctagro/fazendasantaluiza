71e62d93b0895849e98fd9c34b5cc8d0cea68026
fazesantaluiza <fazesantaluiza@gmail.com>
2022-04-15 17:11:19 -0300

    fazendasantaluiza teste de erro linux

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,12 +22,14 @@ class RedirectIfAuthenticated
     {
         $guards = empty($guards) ? [null] : $guards;
 
+dd($guards);
+
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 return redirect(RouteServiceProvider::HOME);
             }
         }
-
+dd('aqui');
         return $next($request);
     }
 }
