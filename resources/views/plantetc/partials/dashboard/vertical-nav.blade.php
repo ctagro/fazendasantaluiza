2a7384b989fb1c4fdeb060222cc71eaceb7b421a
fazesantaluiza <fazesantaluiza@gmail.com>
2023-01-21 17:16:02 -0300

    vyia

--- resources/views/plantetc/partials/dashboard/vertical-nav.blade.php
+++ resources/views/plantetc/partials/dashboard/vertical-nav.blade.php
@@ -20,6 +20,85 @@
         </a>
     </li>
 
+<!----------- Robocar -------->
+
+
+
+<li class="nav-item">
+    <a class="nav-link" data-bs-toggle="collapse" href="#horizontal-menu" role="button" aria-expanded="false" aria-controls="horizontal-menu">
+        <i class="icon">
+            <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path opacity="0.4" d="M10.0833 15.958H3.50777C2.67555 15.958 2 16.6217 2 17.4393C2 18.2559 2.67555 18.9207 3.50777 18.9207H10.0833C10.9155 18.9207 11.5911 18.2559 11.5911 17.4393C11.5911 16.6217 10.9155 15.958 10.0833 15.958Z" fill="currentColor"></path>
+                <path opacity="0.4" d="M22.0001 6.37867C22.0001 5.56214 21.3246 4.89844 20.4934 4.89844H13.9179C13.0857 4.89844 12.4102 5.56214 12.4102 6.37867C12.4102 7.1963 13.0857 7.86 13.9179 7.86H20.4934C21.3246 7.86 22.0001 7.1963 22.0001 6.37867Z" fill="currentColor"></path>
+                <path d="M8.87774 6.37856C8.87774 8.24523 7.33886 9.75821 5.43887 9.75821C3.53999 9.75821 2 8.24523 2 6.37856C2 4.51298 3.53999 3 5.43887 3C7.33886 3 8.87774 4.51298 8.87774 6.37856Z" fill="currentColor"></path>
+                <path d="M21.9998 17.3992C21.9998 19.2648 20.4609 20.7777 18.5609 20.7777C16.6621 20.7777 15.1221 19.2648 15.1221 17.3992C15.1221 15.5325 16.6621 14.0195 18.5609 14.0195C20.4609 14.0195 21.9998 15.5325 21.9998 17.3992Z" fill="currentColor"></path>
+            </svg>
+        </i>
+        <span class="item-name">Vyia</span>
+        <i class="right-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+        </i>
+    </a>
+    <ul class="sub-nav collapse" id="horizontal-menu" data-bs-parent="#sidebar">
+        <li class="nav-item ">
+            <a class="nav-link" href="{{route('panel.index')}}">
+              <i class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                        <g>
+                        <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                        </g>
+                    </svg>
+                </i>
+              <i class="sidenav-mini-icon"> H </i>
+              <span class="item-name"> Painel  </span>
+            </a>
+        </li>
+        <li class=" nav-item ">
+            <a class="nav-link " href="{{route('umidade.index')}}">
+                <i class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                        <g>
+                        <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                        </g>
+                    </svg>
+                </i>
+                <i class="sidenav-mini-icon"> D </i>
+                <span class="item-name">Umidade</span>
+            </a>
+        </li>
+        <li class=" nav-item ">
+            <a class="nav-link" href="{{route('temperatura.create')}}">
+                <i class="icon svg-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                        <g>
+                        <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                        </g>
+                    </svg>
+                </i>
+                <i class="sidenav-mini-icon"> D </i>                   
+                <span class="item-name">Temperatura1</span>
+            </a>
+        </li>
+        
+        <li class="nav-item ">
+            <a class="nav-link" href="">
+                <i class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                        <g>
+                        <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                        </g>
+                    </svg>
+                </i>
+                <i class="sidenav-mini-icon"> B </i>
+                <span class="item-name">Localizações</span>
+            </a>
+        </li>
+    </ul>
+</li>
+
+<!--- Fim  Robocar -->
 
     <ul class="navbar-nav iq-main-menu"  id="sidebar">
         <li class="nav-item static-item">
@@ -28,7 +107,7 @@
                 <span class="mini-icon">-</span>
             </a>
         </li>
-    -->
+   <! ------  Inicio Ceasa   -->
      
  
  <li class="nav-item">
@@ -103,7 +182,15 @@
             </a>
         </li>
     </ul>
--->
+
+
+<!------ Fim Ceasa     -->
+
+<!------ Inicio Pragas e Doenças   -->
+
+
+
+
 <li class="nav-item">
     <a class="nav-link" data-bs-toggle="collapse" href="#horizontal-menu" role="button" aria-expanded="false" aria-controls="horizontal-menu">
         <i class="icon">
@@ -191,6 +278,10 @@
         </a>
     </li>
 </ul>
+
+<!------ Fim Pragas e Doenças   -->
+
+<!------ Inicio Auxiliares   -->
 
 
     <li class="nav-item">
@@ -356,9 +447,13 @@
 
 
 </ul>
-<!--
 
-------
+<!------ Fim  Inicio Auxiliares   -->
+
+
+<!----------- Robocar -------->
+
+
 
 <li class="nav-item">
     <a class="nav-link" data-bs-toggle="collapse" href="#horizontal-menu" role="button" aria-expanded="false" aria-controls="horizontal-menu">
@@ -434,12 +529,12 @@
     </ul>
 </li>
 
--->
+<!--- Fim  Robocar -->
 
 <!-- aqui -->
 
     
-
+<!---- Inicio UI  -----
 
 ---------    Hope UI   ---
 
@@ -548,9 +643,9 @@
         </a>
     </li>
 
-<!-- termina aqui-->
 
-<!-- comeca aquui -->
+
+ comeca aquui 
     <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#sidebar-special-pages" role="button"  aria-expanded="false" aria-controls="sidebar-special-pages">
             <i class="icon">
@@ -1124,5 +1219,7 @@
         </ul>
     </li>
 </ul>
+
+-->
 
 <!-- termina aqui -->
--- resources/views/plantetc/robocar/umidade/index.blade.php
+++ resources/views/plantetc/robocar/umidade/index.blade.php
@@ -3,7 +3,7 @@
       <div class="card">
          <div class="card-header d-flex justify-content-between">
             <div class="header-title">
-               <h4 class="card-title">Umidade do ar</h4>
+               <h4 class="card-title">Umidade do ar  1</h4>
             </div>
          </div>
          <div class="card-body">
